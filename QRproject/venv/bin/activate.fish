95b64a361df0c7cdd3810607b75f8d6e41984cc7
lzhxwmr <lzhxwmr@gmail.com>
2018-02-21 23:18:27 -0500

    icon added

--- QRproject/venv/bin/activate.fish
+++ QRproject/venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/lzh/QRproject/venv"
+set -gx VIRTUAL_ENV "/var/www/Customized-QR/QRproject/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
